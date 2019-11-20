e8689ac0effcd71425537137dbbe08a183383c05
arnandobs <arnandobs20@gmail.com>
2019-11-20 22:37:52 +0700

    my first commit

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import Router from 'vue-router'
 
 const DashboardLayout = () => import(/*webpackChunkName:"dashboard"*/'../components/dashboardLayout.vue')
 
+const LoginLayout = () =>
+    import('../components/loginLayout.vue')
+
+const SignupLayout = () =>
+    import ('../components/signupLayout.vue')
+
 function loadView(view){
     return()=>import(/*webpackChunkName:"view-[request]"*/`../components/dashboardContents/${view}.vue`)
 }
@@ -30,6 +36,16 @@ const routes = [
 
             
         ]
+    },
+    {
+        name: 'LoginLayout',
+        path: '/',
+        component: LoginLayout,
+    },
+    {
+        name: 'SignupLayout',
+        path: '/',
+        component: SignupLayout,
     },
 ]
 Vue.use(Router)
